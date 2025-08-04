5264ef352a2abf220619761f4a1a18a843e87dea
KowdeSindhuja <152280827+KowdeSindhuja@users.noreply.github.com>
2025-08-04 20:57:45 +0530

    Update1 Testfactorial.java

--- Testfactorial.java
+++ Testfactorial.java
@@ -3,9 +3,9 @@ public class Testfactorial {
         try {
             int result = factorial.findFactorial(5);
             if(result == 120) {
-                System.out.println("Test passed ✅");
+                System.out.println("Test passed ");
             } else {
-                System.out.println("Test failed ❌");
+                System.out.println("Test failed ");
             }
         } catch(Exception e) {
             System.out.println("Test error: " + e.getMessage());
